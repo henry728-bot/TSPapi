16e05c4b947d806d2cd29b2c1c424d29a8843d02
Jhonatan Acero <stiddark@gmail.com>
2020-06-11 19:44:38 -0500

    enable type json Middleware

--- app/Http/Middleware/FormatJson.php
+++ app/Http/Middleware/FormatJson.php
@@ -4,7 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 
-class ExampleMiddleware
+class FormatJson
 {
     /**
      * Handle an incoming request.
@@ -15,6 +15,10 @@ class ExampleMiddleware
      */
     public function handle($request, Closure $next)
     {
-        return $next($request);
+
+        if ($request->isJson()) { //
+            return $next($request);
+        }
+        return response()->json(['error' => 'Unauthorized'], 401);
     }
 }
